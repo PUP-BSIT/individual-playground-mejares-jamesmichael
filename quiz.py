cc20b5b9c7e3088acd398bbb4fb92c3f1d97c6f6
mejares-jamesmichael <jamesmichaelmejares@gmail.com>
2024-10-19 20:22:54 +0800

    Add Score System

--- quiz.py
+++ quiz.py
@@ -1,7 +1,7 @@
 #Technewjeans Quiz
 
-print ("(James Michael) Question No. 1: Which of the following is the correct name of my orange tabby cat?\n")
-choice_A = "A) Galaxy Annihilator"
+print ("(James Michael) Question No. 1: Which of the following is the correct name of my orange tabby cat?")
+choice_A = "A) Atomic Matter Devourer"
 choice_B = "B) All Knowing Sentient Being"
 choice_C = "C) Loafy the Gato"
 choice_D = "D) Piping the Orange\n"
@@ -15,14 +15,15 @@ print (choice_D)
 answer = input("Input your choice: ")
 
 if answer == "C" or answer == "c":
-    print ("\nMeowCorrect! Loafy the Gato is the name of my cat")
+    print ("\nCongrats! The correct answer is C")
     print (f"Current Score: {int_score + 1}")
-    int_score = 1
+    int_score =+ 1
 else:
     print ("\nIncorrect! The correct answer is C")
+    print (f"Current Score: {int_score}")
 
 
-print ("\n(James Michael) Question No. 2: When did the Polytechnic University of the Philippines was founded?\n")
+print ("\n(James Michael) Question No. 2: When did the Polytechnic University of the Philippines was founded?")
 choice_A = "A) August 6, 1945"
 choice_B = "B) October 19, 1904"
 choice_C = "C) September 2, 1945"
@@ -37,8 +38,11 @@ answer = input("Input your choice: ")
 
 if answer == "B" or answer == "b":
     
-    print ("\nCorrect! PUP was founded in 1904 with the name of Manila Business School (MBS)")
+    print ("\nCongrats! The correct answer is B")
     print (f"Current Score:  {int_score + 1}")
-    int_score = 2
+    int_score +=1
 else:
     print ("\nIncorrect! The correct answer is B")
+    print (f"Current Score: {int_score}")
+
+print (f"Final Score: {int_score}")
