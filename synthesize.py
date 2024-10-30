fb59af69ac9358ad32e74723a152b31f511f47a6
mejares-jamesmichael <jamesmichaelmejares@gmail.com>
2024-10-30 21:11:27 +0800

    Update Synthesize

--- synthesize.py
+++ synthesize.py
@@ -6,6 +6,16 @@ def process_list(numbers):
 def sum_of_list(numbers):
     return sum(numbers)
 
+# Function to compute average
+def average_of_list(numbers):
+    average = sum(numbers) / len(numbers)
+    return average
+
+# Function to find the highest value
+def highest_value_list(numbers):
+    highest_val = max(numbers)
+    return highest_val
+
 # Create an empty list
 user_list = []
 
@@ -18,8 +28,17 @@ for i in range(5):
 returned_list = process_list(user_list)
 
 # Print the returned list
-print("The returned list is:", returned_list)
+print("\nThe returned list is:", returned_list)
 
+#NUMBER 1 SYNTHESIZE
 # Call the sum_of_list function and print the sum
 total_sum = sum_of_list(returned_list)
 print(f"The total sum of the list is: {total_sum}")
+
+#NUMBER 2 SYNTHESIZE
+total_average = average_of_list (returned_list)
+print(f"The total average of the list is: {total_average}")
+
+#NUMBER 3 SYNTHESIZE
+highest_value = highest_value_list (returned_list)
+print(f"The highest value in the list is: {highest_value}")
